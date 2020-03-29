3b46941b963a38bb0ec979c8f16d7d33f3299451
Fábio Souza <fabio.souza.eti@gmail.com>
2020-03-29 20:32:20 -0300

    Moving examples to repository snes-dev-tools-examples to avoid generate unnecessary image builds

--- docker-local-build.py
+++ docker-local-build.py
@@ -7,8 +7,6 @@ def menu():
     parser = OptionParser()
     parser.add_option("-b", "--build", 
                       help="build local image", action="store_true")
-    parser.add_option("-a", "--run-all-examples",
-                     help="run all examples", action="store_true")
     parser.add_option("-i", "--run-it",
                      help="run interactive", action="store_true")                     
                    
@@ -18,9 +16,7 @@ def menu():
     for option in vars(options):
         value =  getattr(options, option)
         if option == 'build' and value == True:
-            build_image()
-        elif option == 'run_all_examples' and value == True:  
-            run_all_examples()      
+            build_image()   
         elif option == 'run_it' and value == True:  
             run_image()            
 
@@ -30,7 +26,5 @@ def build_image():
 def run_image():
     os.system('docker run -it -v $(pwd)/examples:/tmp/examples fabiosoaza/snes-dev-tools:latest bash')    
 
-def run_all_examples():
-    os.system('docker-compose up -d')
 
 menu()
--- examples/hello-world/build_game.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./build.sh HelloWorld
-superfamicheck HelloWorld.FIG
-
